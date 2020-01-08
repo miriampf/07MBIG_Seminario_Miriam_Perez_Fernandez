5900bb118f9d827d2b7f80f68066533f3335736a
miriampf <miriamperezfernandez@gmail.com>
2020-01-08 14:49:43 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 // Import stylesheets
 import './style.css';
+
+var data = [
+    {"value": 49.9, "name": "Cáncer"},
+    {"value": 27, "name": "Enfermedad cardiovascular"},
+    {"value": 23.1, "name": "Enfermedad respiratoria"}
+  ]
+
+  d3plus.viz()
+    .container("#viz")
+    .data(data)
+    .type("pie")
+    .id("name")
+    .size("value")
+    .draw()
+
+var data2 = [
+    {"value": 46.8, "name": "Cáncer"},
+    {"value": 37.2, "name": "Enfermedad cardiovascular"},
+    {"value": 6, "name": "Enfermedad respiratoria"}
+  ]
+
+  d3plus.viz()
+    .container("#viz2")
+    .data(data2)
+    .type("pie")
+    .id("name")
+    .size("value")
+    .draw()
+
+
+var data3 = [
+    {"value": 65.5, "name": "Cáncer"},
+    {"value": 26.1, "name": "Enfermedad cardiovascular"},
+    {"value": 8.4, "name": "Enfermedad respiratoria"}
+  ]
+
+  d3plus.viz()
+    .container("#viz3")
+    .data(data3)
+    .type("pie")
+    .id("name")
+    .size("value")
+    .draw()
+
+var data4 = [
+    {"value": 60, "name": "Cáncer"},
+    {"value": 25.5, "name": "Enfermedad cardiovascular"},
+    {"value": 14.4, "name": "Enfermedad respiratoria"}
+  ]
+
+  d3plus.viz()
+    .container("#viz4")
+    .data(data4)
+    .type("pie")
+    .id("name")
+    .size("value")
+    .draw()
